4b9c0c765200cae8b73f319f41850829edfa6818
adprm <aditiyaprmn00@gmail.com>
2020-08-30 13:25:29 +0700

    source data table

--- rest-client/application/views/templates/header.php
+++ rest-client/application/views/templates/header.php
@@ -17,6 +17,8 @@
 
   <!-- Custom styles for this template-->
   <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+  <!-- table -->
+  <link href="<?= base_url('assets/'); ?>vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 </head>
 
