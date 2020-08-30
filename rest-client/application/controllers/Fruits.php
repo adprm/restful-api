01669e50743bbf9b1c2d99c831dbb162b252b80a
adprm <aditiyaprmn00@gmail.com>
2020-08-30 13:44:35 +0700

    get all data

--- rest-client/application/controllers/Fruits.php
+++ rest-client/application/controllers/Fruits.php
@@ -3,9 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Fruits extends CI_Controller {
 
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('Fruit_model');
+    }
+
 	public function index()
 	{
         $data['title'] = 'List Fruits';
+        $data['fruits'] = $this->Fruit_model->getAll();
 
 		$this->load->view('templates/header', $data);
 		$this->load->view('templates/sidebar');
