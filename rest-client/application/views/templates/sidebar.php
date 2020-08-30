b59157d36095375b0fdfb73adb162c061b9c4738
adprm <aditiyaprmn00@gmail.com>
2020-08-30 12:51:11 +0700

    add active link

--- rest-client/application/views/templates/sidebar.php
+++ rest-client/application/views/templates/sidebar.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('overview'); ?>">
       <div class="sidebar-brand-icon rotate-n-15">
         <i class="fas fa-apple-alt"></i>
       </div>
@@ -13,8 +13,8 @@
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
+    <li class="nav-item <?php if($this->uri->segment(1)=="overview"){echo "active";}?>">
+        <a class="nav-link" href="<?= base_url('overview'); ?>">
             <i class="fas fa-fw fa-tachometer-alt"></i><span>Dashboard</span>
         </a>
     </li>
