cfeea0e737a1a234eb78c24314765287c505f35b
adprm <aditiyaprmn00@gmail.com>
2020-08-31 18:33:35 +0700

    web name

--- rest-client/application/views/templates/footer.php
+++ rest-client/application/views/templates/footer.php
@@ -2,7 +2,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Your Website 2020</span>
+            <span>Copyright &copy; adprm <?= date('Y'); ?></span>
           </div>
         </div>
       </footer>
